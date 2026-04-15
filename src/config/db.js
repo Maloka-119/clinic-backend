208d0bea3ec6e5d48b7b9f213ed7ebaf06fd56d0
Maloka-119 <aishafathy647@gmail.com>
2026-04-15 16:24:39 +0200

    fix db

--- src/config/db.js
+++ src/config/db.js
@@ -1,23 +1,23 @@
 // src/config/db.js
 const { Sequelize } = require("sequelize");
-const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+require("dotenv").config();
 
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
-// إنشاء الاتصال بقاعدة البيانات
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
+
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+
+  // مهم جدًا لحل مشاكل Railway + Supabase
+  protocol: "postgres",
+  native: false,
 });
-
-// تجربة الاتصال
-// sequelize
-//   .authenticate()
-//   .then(() => console.log("Database connected successfully"))
-//   .catch((err) => console.error("Error connecting to database:", err));
-
-// Note: Database sync is handled in server.js to avoid duplicate sync calls
-// and to have better error handling for constraint and ENUM errors
 
 module.exports = sequelize;
